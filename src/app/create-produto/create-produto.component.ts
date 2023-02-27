3c39c29a098fd0a9b3e2ee10ede7c5839cc20452
mabastos26 <marcio.sgtbastos@gmail.com>
2023-02-27 10:16:37 -0300

    first commit

---
+++ src/app/create-produto/create-produto.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Produto } from "../Produto";
+import { ProdutoService } from "../Produto.service";
+
+@Component({
+    selector: 'app-create-produto',
+    templateUrl: './create-produto.component.html',
+    styleUrls: ['./create-produto.component.css']
+  })
+  export class CreateProdutoComponent implements OnInit {
+    
+  produto: Produto = new Produto();
+  submitted = false;
+
+  constructor(private produtoService: ProdutoService,
+    private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  newProduto(): void {
+    this.submitted = false;
+    this.produto = new Produto();
+  }
+
+  save() {
+    this.produtoService.createProduto(this.produto)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.produto= new Produto();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();    
+  }
+
+  gotoList() {
+    this.router.navigate(['/produtos']);
+  }
+}
